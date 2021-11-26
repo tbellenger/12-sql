159b4e6debadafcccde9ff67f39997943fb693f3
tbellenger <tbellenger@gmail.com>
2021-11-26 15:52:39 -0800

    add update employee role

--- server.js
+++ server.js
@@ -36,14 +36,17 @@ const init = async () => {
         process.exit(0);
     } else {
         if (answer.action == 'view all departments') {
+            // display all the departments
             const [results, fields] = await connection.execute('SELECT * FROM department');
             console.table(results);
             console.log('\n');
         } else if (answer.action == 'view all roles') {
+            // display all the roles - join on the department to get the department name
             const [results, fields] = await connection.execute('SELECT role.id, role.title, role.salary, department.name FROM role LEFT JOIN department on role.department_id = department.id');
             console.table(results);
             console.log('\n');
         } else if (answer.action == 'view all employees') {
+            // display all the employess - join on the role to get title and salary and department to get department name
             const [results, fields] = await connection.execute(`SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.name AS department, CONCAT(manager.first_name,' ', manager.last_name) as manager FROM 
             employee 
             LEFT JOIN role ON employee.role_id = role.id 
@@ -52,6 +55,7 @@ const init = async () => {
             console.table(results);
             console.log('\n');
         } else if (answer.action == 'add a department') {
+            // Get a new department name and insert in the database
             const deptAnswer = await inquirer.prompt([
                 {
                     type: 'input',
@@ -61,6 +65,7 @@ const init = async () => {
             ]);
             const [results, fields] = await connection.execute('INSERT INTO department (name) VALUE (\'' + deptAnswer.newDeptName + '\')');
         } else if (answer.action == 'add a role') {
+            // Get a new role name and insert in the database
             const [results, fields] = await connection.execute('SELECT * FROM department');
             const depts = Array.from(results, x => x.name);
             const roleAnswer = await inquirer.prompt([
@@ -84,10 +89,14 @@ const init = async () => {
             const deptId = results.find(element => element.name == roleAnswer.department);
             const [newRoleResult, newRoleFields] = await connection.execute(`INSERT INTO role (title, salary, department_id) VALUE ('${roleAnswer.newRoleName}','${roleAnswer.salary}',${deptId.id})`);
         } else if (answer.action == 'add an employee') {
+            // Get current employees and roles then 
+            // use that info in the question to add the employee
             const [empResults, empFields] = await connection.execute('SELECT * FROM employee');
             const [roleResults, roleFields] = await connection.execute('SELECT * FROM role');
+            // Pull just the array data needed from the db results
             const roles = Array.from(roleResults, x => x.title);
             const emps = Array.from(empResults, x => x.first_name + ' ' + x.last_name);
+            // add a 'none' option to the list of possible managers
             emps.push('None');
             const empAnswer = await inquirer.prompt([
                 {
@@ -113,9 +122,38 @@ const init = async () => {
                     choices: emps
                 }
             ]);
+            // match up the answers with the id for role and for manager
             const roleId = roleResults.find(element => element.title == empAnswer.role);
             const managerId = empResults.find(element => (element.first_name + ' ' + element.last_name) == empAnswer.manager);
+            // Update the database
             const [newEmpResults, newRoleFields] = await connection.execute(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUE ('${empAnswer.empFirstName}','${empAnswer.empLastName}',${roleId.id},${managerId ? managerId.id : null})`);
+        } else if (answer.action == 'update an employee role') {
+            // Get current employees and roles then 
+            // use that info in the question to update the employee
+            const [empResults, empFields] = await connection.execute('SELECT * FROM employee');
+            const [roleResults, roleFields] = await connection.execute('SELECT * FROM role');
+            // Pull just the array data needed from the db results
+            const roles = Array.from(roleResults, x => x.title);
+            const emps = Array.from(empResults, x => x.first_name + ' ' + x.last_name);
+            
+            const updAnswer = await inquirer.prompt([
+                {
+                    type: 'list',
+                    name: 'emp',
+                    message: 'Which employee would you like to update?',
+                    choices: emps
+                },
+                {
+                    type: 'list',
+                    name: 'role',
+                    message: 'What is the new employee role?',
+                    choices: roles
+                }
+            ]);
+            // match up the answers with the id for role and for manager
+            const roleId = roleResults.find(element => element.title == updAnswer.role);
+            const empId = empResults.find(element => (element.first_name + ' ' + element.last_name) == updAnswer.emp);
+            const [updResult, updFields] = await connection.execute(`UPDATE employee SET role_id=${roleId} WHERE id=${empId}`);
         }
         init();
     }
